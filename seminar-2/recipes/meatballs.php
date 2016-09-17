f89a958b3c935aa0f5b94ab5f61ee3cfb032e0f1
apm <amoosavi@kth.se>
2016-09-18 00:44:44 +0200

    getting recipe information by parsing xml docs

--- seminar-2/recipes/meatballs.php
+++ seminar-2/recipes/meatballs.php
@@ -13,6 +13,10 @@ include_once("../php-mysql-login/connection.php");
 # Setting page where user's at
 $_SESSION['page'] = 0;
 
+# Loading recipe data
+$url = '../resources/recipes/meatballs.xml';
+$xml = simplexml_load_file($url) or die("Can't load file.");
+
 ?>
 
 <!DOCTYPE html>
@@ -59,7 +63,9 @@ and open the template in the editor.
     </div>
     <div class="recipes-list">
         <div class="container">
-            <h2>Turkey Meatballs</h2>
+            <?php foreach ($xml->recipe as $recipe) {
+                echo "<h2>$recipe->title</h2>";
+            } ?>
             <div class="row">
                 <div class="col-md-3">
                     <div class="thumbnail">
@@ -67,32 +73,22 @@ and open the template in the editor.
                     </div>
                     <h4>Ingredients</h4>
                     <ul>
-                        <li>1 small onion, grated</li>
-                        <li>3 garlic cloves, minced</li>
-                        <li>1 large egg</li>
-                        <li>1/4 cup dried bread crumbs</li>
-                        <li>3 tablespoons ketchup</li>
-                        <li>1/4 cup chopped fresh Italian parsley leaves</li>
-                        <li>1/4 cup grated Parmesan</li>
-                        <li>1/4 cup grated Pecorino Romano</li>
-                        <li>1 teaspoon salt</li>
-                        <li>1/4 teaspoon ground black pepper</li>
-                        <li>1 pound ground dark turkey meat</li>
-                        <li>3 tablespoons olive oil</li>
+                        <?php foreach ($recipe->ingredient->children() as $child) {
+                                echo "<li> $child </li>";
+                        } ?>
                     </ul>
-                    <h4>Nutrition summary (calorie breakdown)</h4>
                     <ul>
-                    <li>26% fat</li>
-                    <li>19% carbs</li>
-                    <li>55% protein</li>   
-                    </ul> 
-                    <p>There are 42 calories in 1 medium Turkey Meatball.</p>
+                        <?php foreach ($recipe->nutrition->children() as $child)
+                        {
+                        echo "<li> $child </li>";
+                        }  ?>
+                    </ul>
                 </div>
                 <div class="col-md-5">
                     <h4>Directions</h4>
-                    <p>Add the onion, garlic, egg, bread crumbs, ketchup, parsley, Parmesan, Pecorino, salt and pepper to a large bowl and blend. Mix in the turkey. Shape the turkey mixture into 1 1/4-inch-diameter meatballs. Place on a large plate or baking sheet.</p>
-                    <p>Heat the oil in a heavy large frying pan over medium-high heat. Add the meatballs and saute until browned on all sides, about 5 minutes. Turn off heat. Transfer the meatballs to a plate. Pour off any excess oil. Add the marinara sauce, about 3 cups. Return all the meatballs to the pan. Turn the heat to medium-low and simmer until the sauce thickens slightly and the flavors blend, 15 to 20 minutes. Season the sauce, to taste, with salt and pepper.</p>
-                    <p>Transfer the meatball mixture to a serving bowl. Serve with toothpicks.</p>
+                    <?php foreach ($recipe->recipetext->children() as $child) {
+                        echo "<p>$child </p>";
+                    } ?>
                 </div>
                 <div class="col-md-4">
                     <div class="detailBox">
--- seminar-2/recipes/pancakes.php
+++ seminar-2/recipes/pancakes.php
@@ -12,6 +12,11 @@ include_once("../php-mysql-login/connection.php");
 
 # Setting page where user's at
 $_SESSION['page'] = 1;
+
+# Loading recipe data
+$url = '../resources/recipes/pancakes.xml';
+$xml = simplexml_load_file($url) or die("Can't load file.");
+
 ?>
 
 <!DOCTYPE html>
@@ -57,7 +62,9 @@ and open the template in the editor.
     </div>
     <div class="recipes-list">
         <div class="container">
-            <h2>Honey-Oatmeal Pancakes</h2>
+            <?php foreach ($xml->recipe as $recipe) {
+                echo "<h2>$recipe->title</h2>";
+            } ?>
             <div class="row">
                 <div class="col-md-3">
                     <div class="thumbnail">
@@ -65,35 +72,22 @@ and open the template in the editor.
                     </div>
                     <h4>Ingredients</h4>
                     <ul>
-                    <li>1/2 cup whole wheat flour</li>
-                    <li>1 tablespoon brown sugar</li>
-                    <li>1 teaspoon baking powder</li>
-                    <li>1 teaspoon baking powder</li>
-                    <li>1/2 teaspoon salt</li>
-                    <li>1 1/4 cups quick-cooking rolled oats</li>
-                    <li>1 1/4 cups milk</li>
-                    <li>2 eggs, slightly beaten</li>
-                    <li>3 tablespoons margarine or butter, melted</li>
-                    <li>2 tablespoons honey</li>
-                    <li>Milk (optional)</li>
+                        <?php foreach ($recipe->ingredient->children() as $child) {
+                            echo "<li> $child </li>";
+                        } ?>
                     </ul>
-                    <h4>Nutrition summary (per serving)</h4>
                     <ul>
-                    <li>114 kcal cal.</li>
-                    <li>5 g fat (1 g sat. fat, 37 mg chol.)</li>
-                    <li>1 mg sodium</li>
-                    <li>14 g carb.</li>
-                    <li>1 g fiber</li>
-                    <li>4 g pro</li>    
+                        <?php foreach ($recipe->nutrition->children() as $child)
+                        {
+                            echo "<li> $child </li>";
+                        }  ?>
                     </ul>   
                 </div>
                 <div class="col-md-5">
                     <h4>Directions</h4>
-                    <p>In a large mixing bowl combine whole wheat flour, brown sugar, baking powder, and salt. In another mixing bowl combine rolled oats and milk; let stand 10 minutes.</p>
-                    <p>Add the eggs, margarine or butter, and honey to the oak mixture. Beat until well combined. Add the oat mixture to the flour mixture. Stir just until combined but still slightly lumpy. (If batter is too thick, thin by adding milk, 1 tablespoon at a time, until desired consistency is reached.)</p>
-                     <p>Heat a lightly greased griddle or heavy skillet over medium heat until a few drops of water dance across the surface. For each pancake, pour about 1/4 cup batter onto the hot griddle. Spread batter into a circle about 4 inches in diameter.</p>
-                    <p>Cook over medium heat until pancakes are golden brown, turning to cook the second sides when pancake surfaces are bubbly and edges are slightly dry (about 1 to 2 minutes per side).</p>
-                    <p>Serve immediately or keep warm in a loosely covered ovenproof dish in a 300 degree F oven. Makes 12 pancakes.</p>
+                    <?php foreach ($recipe->recipetext->children() as $child) {
+                        echo "<p>$child </p>";
+                    } ?>
                 </div>
                 <div class="col-md-4">
                     <div class="detailBox">
