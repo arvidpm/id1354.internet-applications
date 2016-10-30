e3c86976e8df3613c5071f0a3f7818fccde5c7e0
apm <amoosavi@kth.se>
2016-10-30 16:01:19 +0100

    disabled persistent connection for seminar 3

--- seminar-3/application/config/database.php
+++ seminar-3/application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => 'id1354-mvc',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => TRUE,
+	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
